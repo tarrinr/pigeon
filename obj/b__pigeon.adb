7d6d5bc2c0677e509f7e50f45ee6abf99fef407c
tarrinr <hitarrin@gmail.com>
2019-05-24 17:13:19 -0600

    .gitignore fix

---
+++ obj/b__pigeon.adb
@@ -0,0 +1,187 @@
+pragma Warnings (Off);
+pragma Ada_95;
+pragma Source_File_Name (ada_main, Spec_File_Name => "b__pigeon.ads");
+pragma Source_File_Name (ada_main, Body_File_Name => "b__pigeon.adb");
+pragma Suppress (Overflow_Check);
+
+package body ada_main is
+
+   E098 : Short_Integer; pragma Import (Ada, E098, "ada__tags_E");
+   E045 : Short_Integer; pragma Import (Ada, E045, "system__soft_links_E");
+   E043 : Short_Integer; pragma Import (Ada, E043, "system__exception_table_E");
+   E112 : Short_Integer; pragma Import (Ada, E112, "system__bb__timing_events_E");
+   E006 : Short_Integer; pragma Import (Ada, E006, "ada__real_time_E");
+   E124 : Short_Integer; pragma Import (Ada, E124, "system__tasking__protected_objects_E");
+   E128 : Short_Integer; pragma Import (Ada, E128, "system__tasking__protected_objects__multiprocessors_E");
+   E133 : Short_Integer; pragma Import (Ada, E133, "system__tasking__restricted__stages_E");
+   E136 : Short_Integer; pragma Import (Ada, E136, "stm32f4__gpiol_E");
+   E135 : Short_Integer; pragma Import (Ada, E135, "stm32f4__rccl_E");
+   E137 : Short_Integer; pragma Import (Ada, E137, "stm32f4__usartl_E");
+   E120 : Short_Integer; pragma Import (Ada, E120, "debug_E");
+
+   Sec_Default_Sized_Stacks : array (1 .. 2) of aliased System.Secondary_Stack.SS_Stack (System.Parameters.Runtime_Default_Sec_Stack_Size);
+
+   Local_Priority_Specific_Dispatching : constant String := "";
+   Local_Interrupt_States : constant String := "";
+
+   Is_Elaborated : Boolean := False;
+
+   procedure adafinal is
+      procedure s_stalib_adafinal;
+      pragma Import (C, s_stalib_adafinal, "system__standard_library__adafinal");
+
+      procedure Runtime_Finalize;
+      pragma Import (C, Runtime_Finalize, "__gnat_runtime_finalize");
+
+   begin
+      if not Is_Elaborated then
+         return;
+      end if;
+      Is_Elaborated := False;
+      Runtime_Finalize;
+      s_stalib_adafinal;
+   end adafinal;
+
+   procedure adainit is
+      Main_Priority : Integer;
+      pragma Import (C, Main_Priority, "__gl_main_priority");
+      Time_Slice_Value : Integer;
+      pragma Import (C, Time_Slice_Value, "__gl_time_slice_val");
+      WC_Encoding : Character;
+      pragma Import (C, WC_Encoding, "__gl_wc_encoding");
+      Locking_Policy : Character;
+      pragma Import (C, Locking_Policy, "__gl_locking_policy");
+      Queuing_Policy : Character;
+      pragma Import (C, Queuing_Policy, "__gl_queuing_policy");
+      Task_Dispatching_Policy : Character;
+      pragma Import (C, Task_Dispatching_Policy, "__gl_task_dispatching_policy");
+      Priority_Specific_Dispatching : System.Address;
+      pragma Import (C, Priority_Specific_Dispatching, "__gl_priority_specific_dispatching");
+      Num_Specific_Dispatching : Integer;
+      pragma Import (C, Num_Specific_Dispatching, "__gl_num_specific_dispatching");
+      Main_CPU : Integer;
+      pragma Import (C, Main_CPU, "__gl_main_cpu");
+      Interrupt_States : System.Address;
+      pragma Import (C, Interrupt_States, "__gl_interrupt_states");
+      Num_Interrupt_States : Integer;
+      pragma Import (C, Num_Interrupt_States, "__gl_num_interrupt_states");
+      Unreserve_All_Interrupts : Integer;
+      pragma Import (C, Unreserve_All_Interrupts, "__gl_unreserve_all_interrupts");
+      Detect_Blocking : Integer;
+      pragma Import (C, Detect_Blocking, "__gl_detect_blocking");
+      Default_Stack_Size : Integer;
+      pragma Import (C, Default_Stack_Size, "__gl_default_stack_size");
+      Default_Secondary_Stack_Size : System.Parameters.Size_Type;
+      pragma Import (C, Default_Secondary_Stack_Size, "__gnat_default_ss_size");
+      Leap_Seconds_Support : Integer;
+      pragma Import (C, Leap_Seconds_Support, "__gl_leap_seconds_support");
+      Bind_Env_Addr : System.Address;
+      pragma Import (C, Bind_Env_Addr, "__gl_bind_env_addr");
+
+      procedure Runtime_Initialize (Install_Handler : Integer);
+      pragma Import (C, Runtime_Initialize, "__gnat_runtime_initialize");
+      Partition_Elaboration_Policy : Character;
+      pragma Import (C, Partition_Elaboration_Policy, "__gnat_partition_elaboration_policy");
+
+      procedure Activate_All_Tasks_Sequential;
+      pragma Import (C, Activate_All_Tasks_Sequential, "__gnat_activate_all_tasks");
+      Binder_Sec_Stacks_Count : Natural;
+      pragma Import (Ada, Binder_Sec_Stacks_Count, "__gnat_binder_ss_count");
+      Default_Sized_SS_Pool : System.Address;
+      pragma Import (Ada, Default_Sized_SS_Pool, "__gnat_default_ss_pool");
+
+   begin
+      if Is_Elaborated then
+         return;
+      end if;
+      Is_Elaborated := True;
+      Main_Priority := 2;
+      Time_Slice_Value := 0;
+      WC_Encoding := 'b';
+      Locking_Policy := 'C';
+      Queuing_Policy := ' ';
+      Task_Dispatching_Policy := 'F';
+      Partition_Elaboration_Policy := 'S';
+      Priority_Specific_Dispatching :=
+        Local_Priority_Specific_Dispatching'Address;
+      Num_Specific_Dispatching := 0;
+      Main_CPU := -1;
+      Interrupt_States := Local_Interrupt_States'Address;
+      Num_Interrupt_States := 0;
+      Unreserve_All_Interrupts := 0;
+      Detect_Blocking := 1;
+      Default_Stack_Size := -1;
+      Leap_Seconds_Support := 0;
+
+      ada_main'Elab_Body;
+      Default_Secondary_Stack_Size := System.Parameters.Runtime_Default_Sec_Stack_Size;
+      Binder_Sec_Stacks_Count := 2;
+      Default_Sized_SS_Pool := Sec_Default_Sized_Stacks'Address;
+
+      Runtime_Initialize (1);
+
+      System.Soft_Links'Elab_Spec;
+      System.Exception_Table'Elab_Body;
+      E043 := E043 + 1;
+      Ada.Tags'Elab_Body;
+      E098 := E098 + 1;
+      System.Bb.Timing_Events'Elab_Spec;
+      E112 := E112 + 1;
+      E045 := E045 + 1;
+      Ada.Real_Time'Elab_Body;
+      E006 := E006 + 1;
+      System.Tasking.Protected_Objects'Elab_Body;
+      E124 := E124 + 1;
+      System.Tasking.Protected_Objects.Multiprocessors'Elab_Body;
+      E128 := E128 + 1;
+      System.Tasking.Restricted.Stages'Elab_Body;
+      E133 := E133 + 1;
+      STM32F4.GPIOL'ELAB_SPEC;
+      E136 := E136 + 1;
+      STM32F4.RCCL'ELAB_SPEC;
+      E135 := E135 + 1;
+      STM32F4.USARTL'ELAB_SPEC;
+      E137 := E137 + 1;
+      DEBUG'ELAB_BODY;
+      E120 := E120 + 1;
+      Activate_All_Tasks_Sequential;
+   end adainit;
+
+   procedure Ada_Main_Program;
+   pragma Import (Ada, Ada_Main_Program, "_ada_pigeon");
+
+   procedure main is
+      procedure Initialize (Addr : System.Address);
+      pragma Import (C, Initialize, "__gnat_initialize");
+
+      procedure Finalize;
+      pragma Import (C, Finalize, "__gnat_finalize");
+      SEH : aliased array (1 .. 2) of Integer;
+
+      Ensure_Reference : aliased System.Address := Ada_Main_Program_Name'Address;
+      pragma Volatile (Ensure_Reference);
+
+   begin
+      Initialize (SEH'Address);
+      adainit;
+      Ada_Main_Program;
+      adafinal;
+      Finalize;
+   end;
+
+--  BEGIN Object file/option list
+   --   C:\Users\trasmussen\Documents\STM\pigeon\obj\stm32f4.o
+   --   C:\Users\trasmussen\Documents\STM\pigeon\obj\stm32f4-gpiol.o
+   --   C:\Users\trasmussen\Documents\STM\pigeon\obj\stm32f4-rccl.o
+   --   C:\Users\trasmussen\Documents\STM\pigeon\obj\stm32f4-usartl.o
+   --   C:\Users\trasmussen\Documents\STM\pigeon\obj\debug.o
+   --   C:\Users\trasmussen\Documents\STM\pigeon\obj\pigeon.o
+   --   -LC:\Users\trasmussen\Documents\STM\pigeon\obj\
+   --   -LC:\Users\trasmussen\Documents\STM\pigeon\obj\
+   --   -LC:\gnat\2018-arm-elf\arm-eabi\lib\gnat\ravenscar-full-stm32f4\adalib\
+   --   -static
+   --   -lgnarl
+   --   -lgnat
+--  END Object file/option list   
+
+end ada_main;
