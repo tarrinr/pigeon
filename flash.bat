b28ddb967b9f9ade30f2e911cb25ef57f313404d
tarrinr <hitarrin@gmail.com>
2019-10-03 16:56:19 -0600

    bin directory changed

--- flash.bat
+++ flash.bat
@@ -1,12 +1,12 @@
 @echo off
 
-echo ### gprbuild ###
-gprbuild
+::echo ### gprbuild ###
+::gprbuild
 
 echo ### arm-eabi-objcopy ###
-arm-eabi-objcopy -O binary obj/pigeon obj/pigeon.bin
+arm-eabi-objcopy -O binary pigeon pigeon.bin
 
 echo ### st-flash ###
-st-flash write obj/pigeon.bin 0x8000000
+st-flash write pigeon.bin 0x8000000
 
-## Use set *0xE000ED0C := 0x05FA0004 to reset board
+:: Use set *0xE000ED0C := 0x05FA0004 to reset board
